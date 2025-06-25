3d97f8fbf53316348d3b9cc6283984285d3893a6
27sssssss <vinniksemen4@gmail.com>
2025-06-25 00:49:44 -0400

    lastchange

--- extractor.py
+++ extractor.py
@@ -3,11 +3,11 @@ def get_data_disasters():
     url = "https://www.gdacs.org/xml/rss.xml" #url from where we take info
     feed = feedparser.parse(url)
     results = []
-    for entry in feed.entries[:10]:
+    for entry in feed.entries[:15]:
         results.append({
             "title" : entry.title,
             "summary": entry.summary,
-            "date": entry.date,
+            "date": entry.published,
             "link": entry.link
         })
     return results
---
+++ main.py
@@ -0,0 +1,15 @@
+from extractor import get_data_disasters
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/disasters")
+def read_disasters():
+    return get_data_disasters()
